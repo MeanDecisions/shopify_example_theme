28429ad659488b45b6b069cee2f0fbcbe6174951
rebeccalaitt <rebecca@charle.co.uk>
2025-02-17 11:57:08 +0000

    klaviyo bis

--- assets/theme.js
+++ assets/theme.js
@@ -2563,7 +2563,6 @@ class SplitWords extends HTMLElement {
     const splitting = Splitting({ target: this, by: 'words' });
 
     splitting[0].words.forEach((item, index) => {
-   
       if (item.textContent.trim() === 'Contemporary') {
         item.style.display = 'none';
         return;
@@ -4032,7 +4031,7 @@ class SliderDots extends HTMLElement {
     this._items = Array.from(this.children);
     this.resetIndexes();
     
-
+    // Rebind click events
     this.items.forEach((item) => {
       item.addEventListener('click', this.onButtonClick.bind(this));
     });
@@ -4515,7 +4514,7 @@ class ProductInfo extends HTMLElement {
         }
       })
       .then(() => {
-        // set focus to last clicked option value
+        
         const activeElement = document.getElementById(targetId);
         if (activeElement.hasAttribute('align-selected')) {
           const scrollElement = activeElement.closest(activeElement.getAttribute('align-selected'));
@@ -4572,7 +4571,7 @@ class ProductInfo extends HTMLElement {
           destination.innerHTML = source.innerHTML;
           destination.removeAttribute('hidden');
           
-        
+  
           if (id === 'VariantMetafieldThumbs') {
             const mediaDots = destination.closest('media-dots');
             if (mediaDots) {
@@ -4618,6 +4617,13 @@ class ProductInfo extends HTMLElement {
       const slider = this.querySelector('slider-element');
       if (slider) {
         slider.reset();
+      }
+
+
+      const buyButtons = this.querySelector('[data-buy-buttons]');
+      const sourceBuyButtons = parsedHTML.querySelector('[data-buy-buttons]');
+      if (buyButtons && sourceBuyButtons) {
+        buyButtons.innerHTML = sourceBuyButtons.innerHTML;
       }
     };
   }
@@ -4715,7 +4721,7 @@ class ProductInfo extends HTMLElement {
   }
 
   updateMedia() {
-    // todo
+ 
   }
 }
 customElements.define('product-info', ProductInfo);
@@ -5382,7 +5388,7 @@ class MediaDots extends SliderDots {
   }
 
   reset() {
-    super.reset();
+    super.reset(); 
     this.resetIndexes();
   }
 
